49c2b067efb6f6dde07b134fa4b71c5867bdddd6
manolo <emanolorb@gmail.com>
2020-08-19 15:18:20 -0500

    Show cover image in detail view

--- app/src/main/java/com/emanolorb/simpleanimelist/ui/DetailActivity.kt
+++ app/src/main/java/com/emanolorb/simpleanimelist/ui/DetailActivity.kt
@@ -6,10 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import coil.api.load
 import com.emanolorb.simpleanimelist.R
 import com.emanolorb.simpleanimelist.common.Tools.debug_print
 import com.emanolorb.simpleanimelist.data.DetailViewModel
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_detail.*
+import java.util.*
 
 class DetailActivity : AppCompatActivity() {
     lateinit var detailViewModel:DetailViewModel
@@ -38,7 +41,17 @@ class DetailActivity : AppCompatActivity() {
         }
     }
     fun setdetails(detail:AnimeModel){
-        debug_print(detail, "detail", true)
+        ivCoverDA.load(detail.attributes.posterImage.medium){
+            //crossfade(true)
+            //placeholder(R.drawable.ic_cinema_color)
+        }
+        tvTitleDA.text = detail.attributes.titles.en_jp
+        tvTypeDA.text = "${getString(R.string.detailTypeDA)}: ${detail.type.capitalize()}"
+        tvStatusDA.text = "${getString(R.string.detailStatusDA)}: ${detail.attributes.status}"
+        tvRatingRankDA.text = "${getString(R.string.detailratingRankDA)}: ${detail.attributes.ratingRank}"
+        tvStartDA.text = "${getString(R.string.detailStartingDateDA)}: ${detail.attributes.startDate}"
+        tvSynopsisDA.text = "${getString(R.string.detailSynopsisDA)}: ${detail.attributes.synopsis}"
+        debug_print(detail, "detail")
     }
 
 }
